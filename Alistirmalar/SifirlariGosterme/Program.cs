b2f69e4142a9cf2f18c1e3234a3d56261f2f8b45
MHusna-Kurs <mehmethusnakurs@gmail.com>
2022-10-04 13:59:49 +0300

    Tek sayıları döndüren proje kodlandı

---
+++ Alistirmalar/SifirlariGosterme/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SifirlariGosterme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> orgSayilar = new List<int> { 1, 2, 3, 4, 5, 0, 6, 0, 7, 0, 8, 9, 0, 10 };
+            SifirlariGosterme(orgSayilar, true);
+            Console.ReadLine();
+        }
+
+        private static void SifirlariGosterme(List<int> sayilar, bool sifirlariGoster)
+        {
+            if (sifirlariGoster == true)
+            {
+                foreach (int sayi in sayilar)
+                {
+                    if (sayi != 0)
+                        Console.Write(sayi + " ");
+                }
+            }
+            else
+            {
+                foreach (int sayi in sayilar)
+                    Console.Write(sayi + " ");
+
+            }
+
+
+        }
+    }
+}
---
+++ Alistirmalar/TekSayilariDondur/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TekSayilariDondur
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[] sayilar = new int[20];
+            int[] tekSayilar = new int[20];
+            Random rnd = new Random();
+
+            for (int i = 0; i < sayilar.Length; i++)
+                sayilar[i] = rnd.Next(0, 50);
+            
+            tekSayilar = TekSayilariDondur(sayilar, tekSayilar);
+
+            foreach (int sayi in tekSayilar)
+                Console.Write(sayi+" ");
+
+            Console.ReadLine();
+        }
+        private static int[] TekSayilariDondur(int[] sayilar, int[] tekSayilar)
+        {
+            int j = 0;
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                if (sayilar[i] % 2 == 1)
+                {
+                    tekSayilar[j] += sayilar[i];
+                    j++;
+                }
+                    
+            }
+            return tekSayilar;
+        }
+    }
+}
