c7eaaf810849ee4e4896a257a92dbca2f63f5999
MHusna-Kurs <mehmethusnakurs@gmail.com>
2022-10-21 10:40:53 +0300

    Textbox'dan alınan içerği listbox'a kayıt eden ugulama.

---
+++ WindowsFormUygulamar/21_10_2022/21_10_2022_v3/21_10_2022_v3/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace _21_10_2022_v3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lbxEntries = new System.Windows.Forms.ListBox();
+            this.tbxEntry = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 38);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Ekle";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lbxEntries
+            // 
+            this.lbxEntries.FormattingEnabled = true;
+            this.lbxEntries.Location = new System.Drawing.Point(118, 12);
+            this.lbxEntries.Name = "lbxEntries";
+            this.lbxEntries.Size = new System.Drawing.Size(120, 199);
+            this.lbxEntries.TabIndex = 1;
+            // 
+            // tbxEntry
+            // 
+            this.tbxEntry.Location = new System.Drawing.Point(12, 12);
+            this.tbxEntry.Name = "tbxEntry";
+            this.tbxEntry.Size = new System.Drawing.Size(100, 20);
+            this.tbxEntry.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 227);
+            this.Controls.Add(this.tbxEntry);
+            this.Controls.Add(this.lbxEntries);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lbxEntries;
+        private System.Windows.Forms.TextBox tbxEntry;
+    }
+}
+
